828156eefa67c7a554a308008d3461d86bf1d6c4
aybrky <ayberk299@gmail.com>
2024-10-09 16:48:30 +0300

    Added video processing and merging features

--- README.md
+++ README.md
@@ -1,24 +1,29 @@
-# YouTube URL to MP3 Converter
+# YouTube URL Converter
 
-This project is a simple YouTube URL to MP3 converter. It takes a YouTube video URL as input and converts the audio track of the video into an MP3 file.
+This project is a YouTube URL converter that allows users to download and convert YouTube videos to various formats, with the option to download audio, video, or both separately. Users can customize the quality and format for each, and the tool also provides an option to merge audio and video.
 
 ## Features
-- Convert YouTube videos to MP3 format.
-- Audio quality selection. 
-- Output format selection.
-- Lightweight and easy to use.
-- Uses `yt-dlp` for downloading and `ffmpeg` for audio extraction.
+- **Download Options:** Choose to download either audio, video, or both from YouTube videos.
+- **Format and Quality Selection:** Customize the output format and quality for both audio and video.
+- **Merging Capability:** Merge selected audio and video files into a single output if it is needed.
+- **Flexible Output:** Supports downloading files in different combinations, allowing users to select specific formats and qualities for audio and video separately.
+- **Lightweight and Easy to Use:** Simple, user-friendly interface for quick and efficient conversions.
+- **Powerful Tools:** Uses `yt-dlp` for downloading and `FFmpeg` for both audio and video processing. 
   
+### Supported Formats
+- MP3, MP4, AVI, WAV, M4A, FLAC, OGG, MKV, MOV, WEBM, FLV and MPEG.
+
 ## Requirements
 - Python 3.x
 - [yt-dlp](https://github.com/yt-dlp/yt-dlp)
 - [ffmpeg](https://ffmpeg.org/download.html) (required for extracting audio)
+- subprocess (Built-in)
 
 ## Installation
 
 ### 1. Clone the repository:
 ```bash
-git clone https://github.com/code-freq/youtube-url-to-mp3-converter.git
+git clone https://github.com/code-freq/youtube-url-converter.git
 ```
 ### 2. Install the required packages:
 ```bash
@@ -30,13 +35,29 @@ Follow the instructions [here](https://ffmpeg.org/download.html) to install ffmp
 **_Ensure that `ffmpeg` is available in your system's PATH so that it can be used by `yt-dlp` during conversion._**
 
 ## Usage
-Run `main.py` and enter the Youtube URL.
-The script will download the video and extract the audio into MP3 format, saving it in the current directory.
+1. Run `main.py`
+2. Enter the YouTube URL.
+3. Select the audio quality (or type none to skip audio download).
+4. If you chose an audio quality, select the desired audio format.
+5. Select the video quality (or type none to skip video download).
+6. If you chose a video quality, select the desired video format.
+7. If both audio and video are selected, choose whether to merge them into a single file.
+8. Merged file will be downloaded to the current directory, with the file name formatted as <title> (merged).<ext>.
 
 ## How It Works
-- The script uses `yt-dlp` to download the YouTube video.
-- Audio is extracted from the video in MP3 format using `ffmpeg`.
-- Output MP3 files are saved to the current directory with the video title as the filename.
+1. **YouTube Video Downloading:** The script utilizes `yt-dlp` to download the specified YouTube video based on the provided URL, ensuring that users have access to the latest video formats and quality options.
+2. **Format and Quality Selection:** Users can customize their downloads by selecting the desired formats and quality for both audio and video through the options provided by `yt-dlp`, allowing for tailored outputs based on user preferences.
+3. **Audio and Video Processing:** The script processes audio and video separately using `FFmpeg`, preparing each for conversion based on the selected formats.
+4. **Merging:** `FFmpeg` is employed to merge the two files into a single output when required, ensuring high-quality results.
 
 > [!IMPORTANT]
 > If `ffmpeg` is not installed or not properly added to the PATH, the audio extraction will not work.
+
+> [!TIP]
+> **Use VLC for Playback:** It is recommended to use VLC Media Player for the best compatibility and playback of the downloaded audio and video files. VLC supports a wide range of formats and ensures smooth playback without issues.
+> **Handling Temporary Server Errors:** Due to occasional discrepancies with YouTube's servers, audio and video quality options may differ. If this occurs, restarting the program a few times may help resolve the issue and improve the quality of the downloads.
+> **Experiment with Different Formats:** When merging files, certain format selections may lead to security errors or application crashes. It is advisable to try different formats if you encounter any issues during the merging process to find the best combination that works for your needs.
+
+## Contact
+
+For suggestions, recommendations, development ideas, or any issues, feel free to reach out at [here](code.freq7@gmail.com).
--- main.py
+++ main.py
@@ -1,72 +1,263 @@
 import yt_dlp
+import subprocess
 
-def fetch_audio(url, opts):
+
+# FUNCTIONS --------------------------------------------------------------------------
+
+# download the file at the stream url with the options given
+def fetch(url, opts):
     with yt_dlp.YoutubeDL(opts) as ydl:
         ydl.download([url])
     print("Download complete")
 
-# list available audio formats, print them and get user selection
-def format_selection(url) -> (str, str):
-    available_formats = []
+# get info dict of all formats
+def get_info(url) -> dict:
     with yt_dlp.YoutubeDL({"format":"all"}) as ydl:
-        info_dict = ydl.extract_info(url, download=False)
-        duration = info_dict["duration"]
-        mins = duration // 60
-        secs = duration % 60
-        duration = f"Duration: {mins} m, {secs} s"
+        inf_dict = ydl.extract_info(url, download=False)
+    return inf_dict
 
-        formats = info_dict.get("formats", [])
+# list available audio formats, print them and get user selection
+def audio_format_selection(info_dict) -> (str, str, str):
+    available_audio_formats = []
 
-        print("\nAvailable formats:"
-              "########################################################################")
-        for f in formats:
-            resolution = f["resolution"]
-            if resolution == "audio only" and "asr" in f.keys():
-                available_formats.append(f)
+    duration = info_dict["duration"]
+    hours = duration // 3600
+    mins = duration // 60
+    secs = duration % 60
+    duration = f"Duration: {hours} h, {mins} m, {secs} s\n"
 
-                print(f"Format ID: {f["format_id"]}  Audio Sampling Rate: {f["asr"]}  File Size: {round(f["filesize"]/1024, 2)} KB"
-                      f"\nQuality: {f["quality"]}  Total Bit Rate: {f["tbr"]}  Codec: {f["acodec"]}  Extension: {f["audio_ext"]}")
-                print("------------------------------------------------------------------------")
+    formats = info_dict.get("formats", [])
 
-        # gather ID's
-        ids = []
-        [ids.append(i["format_id"]) for i in available_formats]
+    # list available audio formats
+    print("\nAvailable audio formats:\n"
+          "########################################################################")
+    for f in formats:
+        resolution = f["resolution"]
+        if resolution == "audio only" and not f.get("asr") is None:
+            available_audio_formats.append(f)
 
-        # user input error handling loop
-        while True:
-            user_f_select = input("Please select format ID: ")
-            if user_f_select not in ids:
-                print("Invalid format ID")
-                continue
-            break
+            print(f"Format ID: {f["format_id"]}  Audio Sampling Rate: {f["asr"]}  File Size: {round(f["filesize"]/1024, 2)} KB"
+                  f"\nQuality: {f["quality"]}  Average Bit Rate: {f["abr"]}  Codec: {f["acodec"]}  Extension: {f["audio_ext"]}")
+            print("------------------------------------------------------------------------")
 
-    return user_f_select, duration
+    # gather audio format ID's
+    audio_ids = []
+    [audio_ids.append(i["format_id"]) for i in available_audio_formats]
+    audio_ids.append("none")
 
-url_input = input("Enter the video url: ").strip(" ")
+    # user input error handling loop
+    selected_codec = None
+    while True:
+        user_af_select = input("Please select audio format ID (Enter 'none' for no audio): ")
+        if user_af_select not in audio_ids:
+            print("Invalid format ID")
+            continue
+        # get codec of the selected format
+        selected_codec = next((i["acodec"].split(".", 1)[0] for i in available_audio_formats if str(i["format_id"]) == user_af_select), None)
+        break
 
-format_choice, duration_str = format_selection(url_input)
+    return user_af_select, selected_codec, duration
+
+# list available video formats, print them and get user selection
+def video_format_selection(info_dict) -> (str, str, str):
+    available_video_formats = []
+
+    duration = info_dict["duration"]
+    hours = duration // 3600
+    mins = duration // 60
+    secs = duration % 60
+    duration = f"Duration: {hours} h, {mins} m, {secs} s"
+
+    formats = info_dict.get("formats", [])
+
+    # list available video formats
+    print("\nAvailable video formats:\n"
+          "########################################################################")
+    for f in formats:
+        if "asr" in f.keys() and f["asr"] is None:  # get only video formats
+            available_video_formats.append(f)
+
+            print(
+                f"Format ID: {f["format_id"]}  File Size: {round(f["filesize"] / (1024 * 1024), 2)} MB"
+                f"\nQuality: {f["quality"]}  Average Video Bit Rate: {f["vbr"]}  Codec: {f["vcodec"]}  "
+                f"Extension: {f["video_ext"]}  Resolution: {f["resolution"]} ({f["format_note"]})")
+            print("------------------------------------------------------------------------")
+
+    # gather video format ID's
+    video_ids = []
+    [video_ids.append(i["format_id"]) for i in available_video_formats]
+    video_ids.append("none")
+
+    # user input error handling loop
+    selected_codec = None
+    while True:
+        user_vf_select = input("Please select video format ID (Enter 'none' for no video): ")
+        if user_vf_select not in video_ids:
+            print("Invalid format ID")
+            continue
+        # get codec of the selected format
+        selected_codec = next((i["vcodec"].split(".",1)[0] for i in available_video_formats if str(i["format_id"]) == user_vf_select), None)
+        break
+
+    return user_vf_select, selected_codec, duration
+
+# merge video and audio
+def merge_video_audio(video_file, audio_file, output_file):
+    command = [
+        'ffmpeg', '-i', video_file, '-i', audio_file,
+        '-c:v', 'copy', '-c:a', 'aac', output_file
+    ]
+    subprocess.run(command)
+
+# MAIN ---------------------------------------------------------------------------------
+
+url_input = input("Enter the video url: ").strip(" ")  # get URL
+info_dict_ = get_info(url_input)  # get info dict
+vcodec_choice = None
+
+# AUDIO PART ---------------------------------------------------------------------------
+af_choice, codec, duration_str = audio_format_selection(info_dict_)
 print(duration_str)
 
-# Audio file formats list that FFmpeg supports
-output_formats = ["mp3", "aac", "m4a", "opus", "vorbis", "flac", "alac", "wav"]
+if not af_choice == "none":
+    user_query = None
+    audio_output_formats = None
 
-# user input error handling loop
-while True:
-    codec_choice = input("Which audio file format do you want to convert to (e.g., mp3, m4a, wav): ")
-    if not codec_choice in output_formats:
-        print("Invalid output format")
-        continue
-    break
+    # specify available output formats and get user input in order to selected codec
+    if codec == "mp4a":
+        user_query = (" codec --> format\n"
+                      "   mp3 --> mp3\n"
+                      "   wav --> wav\n"
+                      "   m4a --> m4a\n"
+                      "  none --> m4a\n"
+                      "   aac --> m4a\n"
+                      "  alac --> m4a\n"
+                      "  flac --> flac\n"
+                      "vorbis --> ogg")
 
-# create options
-ydl_opts = {
-        'format': str(format_choice),  # Download the desired format
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',  # Use FFmpeg to extract audio
-            'preferredcodec': str(codec_choice),  # Convert to desired output format
-        }],
-        'outtmpl': '%(title)s.%(ext)s',  # Save with the video title
-    }
+        # Audio file formats list that mp4a codec is compatible with
+        audio_output_formats = ["mp3", "wav", "m4a", "flac", "vorbis", "alac", "none"]
 
-fetch_audio(url_input, ydl_opts)
-#https://www.youtube.com/watch?v=JcoKMe0XyqY
+    elif codec == "opus":
+        user_query = ("(codec --> format)\n"
+                      "   wav --> wav\n"
+                      "   mp3 --> mp3\n"
+                      "   m4a --> m4a\n"
+                      "   aac --> m4a\n"
+                      "  none --> webm\n"
+                      "  flac --> flac\n")
+
+        # Audio file formats list that mp4a codec is compatible with
+        audio_output_formats = ["wav", "flac", "aac", "m4a", "none", "mp3"]
+
+    else:
+        print("Unrecognized video file extension..")
+        exit()
+
+    # print user query text
+    print(f"Which audio file format do you want to convert to?\n"
+          f"In order to format you want, select output codec please.\n"
+          f"{user_query}"
+          f"('none' for no conversion)")
+
+    # user input error handling loop
+    while True:
+        acodec_choice = input(":")
+        if not acodec_choice in audio_output_formats:
+            print("Invalid output format")
+            continue
+        break
+
+    # create options
+    if acodec_choice == "none":
+        audio_ydl_opts = {
+            'format': af_choice,  # Download the desired format
+            'outtmpl': '%(title)s.%(ext)s',  # Save with the video title
+        }
+    else:
+        audio_ydl_opts = {
+                'format': af_choice,  # Download the desired format
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',  # Use FFmpeg to extract audio
+                    'preferredcodec': acodec_choice,  # Convert to desired output format
+                }],
+                'outtmpl': '%(title)s.%(ext)s',  # Save with the video title
+            }
+
+    fetch(url_input, audio_ydl_opts)  # fetch and download
+
+# VIDEO PART ----------------------------------------------------------------------------
+vf_choice, codec, duration_str = video_format_selection(info_dict_)
+print(duration_str)
+
+if not vf_choice == "none":
+    user_query = None
+    video_output_formats = None
+
+    # specify available output formats and get user input in order to selected extension
+    if codec == "av01":
+        user_query = "mp4, webm, ogg, mkv, avi, mov, flv, mpeg"
+
+        # Video file formats list that 'av1' codec is compatible with
+        video_output_formats = ["mp4", "webm", "ogg", "mkv", "avi", "mov", "flv", "mpeg"]
+
+    elif codec == "avc1" or codec == "vp09":
+        user_query = "mp4, webm, mkv, avi, mov, flv, mpeg"
+
+        # Video file formats list that 'avc' and 'vp9' codecs are compatible with
+        video_output_formats = ["mp4", "webm", "mkv", "avi", "mov", "flv", "mpeg"]
+
+    elif codec == "vp08":  # for old youtube videos
+        user_query = "webm"
+
+        # Video file formats list that 'vp8' codec is compatible with
+        video_output_formats = ["webm"]
+
+    else:
+        print("Unrecognized video file extension..")
+        exit()
+
+    print(f"Which video file format do you want to convert to ({user_query})")
+
+    # user input error handling loop
+    while True:
+        vcodec_choice = input(":")
+        if not vcodec_choice in video_output_formats:
+            print("Invalid output format")
+            continue
+        break
+
+    # create options
+    video_ydl_opts = {
+            'format': vf_choice,  # Download the desired format
+            'postprocessors': [{
+                'key': 'FFmpegVideoConvertor',  # Use FFmpeg to extract audio
+                'preferedformat': vcodec_choice,  # Convert to desired output format
+            }],
+            'outtmpl': '%(title)s.%(ext)s',  # Save with the video title
+        }
+
+
+    fetch(url_input, video_ydl_opts)  # fetch and download
+
+# MERGING PART --------------------------------------------------------------------------
+
+# get user permission with error handle loop
+if not vf_choice == "none" and not af_choice == "none":
+    print("Do you want to merge these files? (y/n)")
+    while True:
+        user_perm = input(":")
+        if user_perm.lower() == "y":
+            merge_ydl_opts = {
+                'format': f"{vf_choice}+{af_choice}",  # Select the chosen audio and video format
+                'outtmpl': '%(title)s (merged).%(ext)s',  # Save with the video title
+                'ext': vcodec_choice,  # Save merged format as selected video format
+            }
+            fetch(url_input, merge_ydl_opts)  # fetch and download
+            break
+        elif user_perm.lower() == "n":
+            break
+        else:
+            print("Invalid input")
+            continue
+
